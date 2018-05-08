8eaf297358130ca7e8355014443489a66904b0a5
justinhunt <bitwalkerjapan@gmail.com>
2018-05-08 15:00:04 +0900

    updates to beta GDPR code

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -33,7 +33,7 @@ use \core_privacy\local\metadata\collection;
 use \core_privacy\local\metadata\provider as metadataprovider;
 use \core_privacy\local\request\writer;
 use \core_privacy\local\request\contextlist;
-use \mod_assign\privacy\submission_request_data;
+use \mod_assign\privacy\assign_plugin_request_data;
 
 /**
  * Privacy Subsystem for assignsubmission_onlinepoodll implementing null_provider.
@@ -46,7 +46,7 @@ use \mod_assign\privacy\submission_request_data;
 ///  use \core_privacy\local\legacy_polyfill;
 ///use \mod_assign\privacy\assignsubmission_provider\legacy_polyfill;
 
-class provider implements metadataprovider {
+class provider implements metadataprovider, \mod_assign\privacy\assignsubmission_provider {
     use \core_privacy\local\legacy_polyfill;
 
 
@@ -61,13 +61,14 @@ class provider implements metadataprovider {
         $collection->link_subsystem('core_files', 'privacy:metadata:filepurpose');
         return $collection;
     }
+
     /**
      * This is covered by mod_assign provider and the query on assign_submissions.
      *
      * @param  int $userid The user ID that we are finding contexts for.
      * @param  contextlist $contextlist A context list to add sql and params to for contexts.
      */
-    public static function _get_context_for_userid_within_submission($userid, contextlist $contextlist) {
+    public static function get_context_for_userid_within_submission(int $userid, contextlist $contextlist) {
         // This is already fetched from mod_assign.
     }
     /**
@@ -84,18 +85,21 @@ class provider implements metadataprovider {
      * @param  submission_request_data $exportdata Data used to determine which context and user to export and other useful
      * information to help with exporting.
      */
-    public static function _export_submission_user_data(submission_request_data $exportdata) {
+    public static function export_submission_user_data(assign_plugin_request_data $exportdata) {
         // We currently don't show submissions to teachers when exporting their data.
         $context = $exportdata->get_context();
         if ($exportdata->get_user() != null) {
             return null;
         }
         $user = new \stdClass();
-        $plugin = $exportdata->get_subplugin();
-        $files = $plugin->get_files($exportdata->get_submission(), $user);
+        $assign = $exportdata->get_assign();
+        $plugin = $assign->get_plugin_by_type('assignsubmission', 'onlinepoodll');
+
+        $files = $plugin->get_files($exportdata->get_pluginobject(), $user);
         foreach ($files as $file) {
-            $userid = $exportdata->get_submission()->userid;
+            $userid = $exportdata->get_pluginobject()->userid;
             writer::with_context($exportdata->get_context())->export_file($exportdata->get_subcontext(), $file);
+
             // Plagiarism data.
             $coursecontext = $context->get_course_context();
             \core_plagiarism\privacy\provider::export_plagiarism_user_data($userid, $context, $exportdata->get_subcontext(), [
@@ -106,33 +110,53 @@ class provider implements metadataprovider {
             ]);
         }
     }
+
     /**
      * Any call to this method should delete all user data for the context defined in the deletion_criteria.
      *
      * @param  submission_request_data $requestdata Information useful for deleting user data.
      */
-    public static function _delete_submission_for_context(submission_request_data $requestdata) {
+    public static function delete_submission_for_context(assign_plugin_request_data $requestdata) {
         global $DB;
+
         \core_plagiarism\privacy\provider::delete_plagiarism_for_context($requestdata->get_context());
+
         $fs = get_file_storage();
         $fs->delete_area_files($requestdata->get_context()->id, ASSIGNSUBMISSION_ONLINEPOODLL_COMPONENT, ASSIGNSUBMISSION_ONLINEPOODLL_FILEAREA);
-        // Delete records from assignsubmission_file table. -- Could use the assignment id.....
+
+        // Delete records from assignsubmission_file table.
         $DB->delete_records(ASSIGNSUBMISSION_ONLINEPOODLL_TABLE, ['assignment' => $requestdata->get_assign()->get_instance()->id]);
     }
+
+
+
     /**
      * A call to this method should delete user data (where practical) using the userid and submission.
      *
      * @param  submission_request_data $exportdata Details about the user and context to focus the deletion.
      */
-    public static function _delete_submission_for_userid(submission_request_data $exportdata) {
+    public static function delete_submission_for_userid(assign_plugin_request_data $deletedata) {
         global $DB;
-        \core_plagiarism\privacy\provider::delete_plagiarism_for_user($exportdata->get_user()->id, $exportdata->get_context());
-        $submissionid = $exportdata->get_submission()->id;
+
+        \core_plagiarism\privacy\provider::delete_plagiarism_for_user($deletedata->get_user()->id, $deletedata->get_context());
+
+        $submissionid = $deletedata->get_pluginobject()->id;
+
         $fs = get_file_storage();
-        $fs->delete_area_files($exportdata->get_context()->id, ASSIGNSUBMISSION_ONLINEPOODLL_COMPONENT, ASSIGNSUBMISSION_ONLINEPOODLL_FILEAREA,
+        $fs->delete_area_files($deletedata->get_context()->id, ASSIGNSUBMISSION_ONLINEPOODLL_COMPONENT, ASSIGNSUBMISSION_ONLINEPOODLL_FILEAREA,
             $submissionid);
-        $DB->delete_records(ASSIGNSUBMISSION_ONLINEPOODLL_TABLE, ['assignment' => $exportdata->get_assign()->get_instance()->id,
+
+        $DB->delete_records(ASSIGNSUBMISSION_ONLINEPOODLL_TABLE, ['assignment' => $deletedata->get_assign()->get_instance()->id,
             'submission' => $submissionid]);
+    }
+
+    /**
+     * This is also covered by the mod_assign provider and it's queries.
+     *
+     * @param  \mod_assign\privacy\useridlist $useridlist An object for obtaining user IDs of students.
+     */
+    public static function get_student_user_ids(\mod_assign\privacy\useridlist $useridlist) {
+        // No need.
     }
 
 
