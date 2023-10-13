71e3511684104beef9a81ff1733a3a1400e5d9b0
justinhunt <bitwalkerjapan@gmail.com>
2023-10-13 08:05:50 +0000

    version bump

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 
 
-$plugin->version   = 2021111100;
+$plugin->version   = 2023101300;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component =  'assignsubmission_onlinepoodll';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.1.11 (Build 2021111100)';
+$plugin->release   = '3.1.12 (Build 2023101300)';
 $plugin->dependencies = array('filter_poodll' => 2018012001);
